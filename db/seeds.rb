e442f8ae199e0338a7c5bac40ace1c1e5f3e6f7f
EC2 Default User <ec2-user@ip-172-31-14-230.us-east-2.compute.internal>
2018-08-20 23:12:07 +0000

    adding faker, identifying fake users, and adding fake users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+puts "Generating my user"
+
 u = User.find_or_create_by(username: "objo")
 u.update_attributes({
     first_name: "Mike",
@@ -5,6 +7,27 @@ u.update_attributes({
     city: "Columbus",
     state: "Ohio",
     bio:   "lorem",
+    fake:  false,
     })
     
 u.save!
+
+puts "Deleting fake users"
+User.where(fake: true).destroy_all
+
+puts "Generating fake users\n"
+10.times do
+    putc "."
+    User.create!({
+        username: Faker::Internet.username,
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        city: Faker::Address.city,
+        state: Faker::Address.state_abbr,
+        bio: Faker::Lorem.paragraph,
+        fake: true,
+    })
+    
+end
+
+puts "done"
